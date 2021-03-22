dea6e1a49c589476f8d08122792f362c0450dac3
Enrip <50412350+Enrip99@users.noreply.github.com>
2021-03-22 13:29:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ client.on('message', message => {
 
 
 else if (ms === '7f'){
-  message.channel.send('** **- Para ver una foto del despacho, escribe `alguien despacho?` y espera un segundo.\n Para apagar el bot utiliza el comando `shut off`\n - Otros comandos graciosos son: `ping`, `mistetas`, `nep`, `upclink`.\n - Este bot ha sido desarrollado y mantenido por un gilipollas - https://github.com/Enrip99/DespachoBot-Discord');
+  message.channel.send('** **- Para ver una foto del despacho, escribe `alguien despacho?` y espera un segundo.\n - Para apagar el bot utiliza el comando `shut off`\n - Otros comandos graciosos son: `ping`, `mistetas`, `nep`, `upclink`.\n - Este bot ha sido desarrollado y mantenido por un gilipollas - https://github.com/Enrip99/DespachoBot-Discord');
 }
 
 
